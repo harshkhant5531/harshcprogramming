4d0fcccd184db7c010f49bd4fcf06fd6d178599b
harshkhant5531 <143692243+harshkhant5531@users.noreply.github.com>
2023-10-05 12:47:26 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	
+	char alphabet,a,e,i,o,u;
+	printf("enter an alphabet:");
+	scanf("%c",&alphabet);
+	
+	if(alphabet==a||alphabet==e||alphabet==i||alphabet==o||alphabet==u){
+		printf("alphabet is vowel:%c",alphabet);
+		
+	}
+	else{
+		printf("alphabet is constunt:%c",alphabet);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
